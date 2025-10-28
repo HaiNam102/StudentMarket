b2688b85f7589d50dd0935ba06f1e7947483d414
Hai Nam <bimhuongday@gmail.com>
2025-10-28 11:42:08 +0700

    add ChildCategoryController

--- studentmarket-main/studentmarket/src/main/java/com/postgresql/StudentMarket/Controllers/ChildCategoryController.java
+++ studentmarket-main/studentmarket/src/main/java/com/postgresql/StudentMarket/Controllers/ChildCategoryController.java
@@ -5,13 +5,8 @@ import com.postgresql.StudentMarket.Services.ChildCategoryService;
 import lombok.AccessLevel;
 import lombok.RequiredArgsConstructor;
 import lombok.experimental.FieldDefaults;
-import org.springframework.http.ResponseEntity;
-import org.springframework.stereotype.Controller;
-import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.ControllerAdvice;
-import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
-import org.springframework.web.bind.annotation.ResponseBody;
 
 import java.util.List;
 
